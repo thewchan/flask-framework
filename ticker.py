88b0968e8e35687242639cea476f0621d6f4886f
Matt Chan <thewchan@gmail.com>
2020-06-25 14:37:35 -0700

    Wrapped ticker.py as module.

--- ticker.py
+++ ticker.py
@@ -9,85 +9,88 @@ from bokeh.models import HoverTool
 from bokeh.models.tickers import DatetimeTicker
 
 
-KEY = 'Q7JC95BEFD22FQDA'
-
-
-def get_data(symbol):
-    """Get stock data given symbol from api, return json."""
-    url = (f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&"
-           f"symbol={symbol}&apikey={KEY}")
-    response = requests.get(url)
-    data = response.json()
-
-    return data
-
-
-def get_df(data):
-    """Return formatted dataframe from json data."""
-    df = pd.DataFrame(data['Time Series (Daily)']).T
-    df.index = pd.to_datetime(df.index)
-    df = df.sort_index().copy()
-    df.columns = df.columns.str.replace(r'\d+\.\s', '')
-    df.columns = map(str.title, df.columns)
-    df = df.drop('Volume', axis=1).astype(float).copy()
-
-    return df
-
-
 def ticker(symbol):
-    """Render the boken html file."""
-    data = get_data(symbol)
-    df = get_df(data)
+    """Build stock ticker plot, return it to outside module."""
+    KEY = 'Q7JC95BEFD22FQDA'
 
-    monthago = df.index[-20]
-    now = df.index[-1]
-    high = df.loc[monthago:now].max().max() + 5
-    low = df.loc[monthago:now].min().min() - 5
+    def get_data(symbol):
+        """Get stock data given symbol from api, return json."""
+        url = (f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&"
+               f"symbol={symbol}&apikey={KEY}")
+        response = requests.get(url)
+        data = response.json()
 
-    p = bkp.figure(title=f'{symbol} Stock Price',
-                   y_axis_label='Dollars',
-                   x_axis_type='datetime',
-                   plot_width=800,
-                   plot_height=400,
-                   x_range=(monthago, now),
-                   y_range=(low, high),
-                   toolbar_location="below",
-                   tools="pan,box_zoom,wheel_zoom,reset,save",
-                   active_drag="pan",
-                   )
-    p.circle(df.index,
-             df['Open'],
-             size=8,
-             color='olive',
-             alpha=0.6,
-             legend_label='Open',
-             )
-    p.circle(df.index,
-             df['Close'],
-             size=8,
-             color='orange',
-             alpha=0.6,
-             legend_label='Close',
-             )
-    p.line(df.index,
-           df['High'],
-           line_width=2,
-           color='steelblue',
-           legend_label='High',
-           )
-    p.line(df.index,
-           df['Low'],
-           line_width=2,
-           color='firebrick',
-           legend_label='Low',
-           )
-    p.xaxis.ticker = DatetimeTicker(desired_num_ticks=20)
-    hover = HoverTool(tooltips=[('Date', '@x{%F}'),
-                                ('Price', '$@y{0.00 a}'),
-                                ],
-                      formatters={'@x': 'datetime'})
-    p.add_tools(hover)
-    p.toolbar.active_inspect = hover
+        return data
+
+    def get_df(data):
+        """Return formatted dataframe from json data."""
+        df = pd.DataFrame(data['Time Series (Daily)']).T
+        df.index = pd.to_datetime(df.index)
+        df = df.sort_index().copy()
+        df.columns = df.columns.str.replace(r'\d+\.\s', '')
+        df.columns = map(str.title, df.columns)
+        df = df.drop('Volume', axis=1).astype(float).copy()
+
+        return df
+
+    def build_plot(symbol):
+        """Render the boken html file."""
+        data = get_data(symbol)
+        df = get_df(data)
+
+        monthago = df.index[-20]
+        now = df.index[-1]
+        high = df.loc[monthago:now].max().max() + 5
+        low = df.loc[monthago:now].min().min() - 5
+
+        p = bkp.figure(title=f'{symbol} Stock Price',
+                       y_axis_label='Dollars',
+                       x_axis_type='datetime',
+                       plot_width=800,
+                       plot_height=400,
+                       x_range=(monthago, now),
+                       y_range=(low, high),
+                       toolbar_location="below",
+                       tools="pan,box_zoom,wheel_zoom,reset,save",
+                       active_drag="pan",
+                       )
+        p.circle(df.index,
+                 df['Open'],
+                 size=8,
+                 color='olive',
+                 alpha=0.6,
+                 legend_label='Open',
+                 )
+        p.circle(df.index,
+                 df['Close'],
+                 size=8,
+                 color='orange',
+                 alpha=0.6,
+                 legend_label='Close',
+                 )
+        p.line(df.index,
+               df['High'],
+               line_width=2,
+               color='steelblue',
+               legend_label='High',
+               )
+        p.line(df.index,
+               df['Low'],
+               line_width=2,
+               color='firebrick',
+               legend_label='Low',
+               )
+        p.xaxis.ticker = DatetimeTicker(desired_num_ticks=20)
+        hover = HoverTool(tooltips=[('Date', '@x{%F}'),
+                                    ('Price', '$@y{0.00 a}'),
+                                    ],
+                          formatters={'@x': 'datetime'})
+        p.add_tools(hover)
+        p.toolbar.active_inspect = hover
+
+        return p
+
+    p = build_plot(symbol)
 
     return p
 
