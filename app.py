4ba9ebd6760e760900cdcff9e79d0b531aa9fcbd
Matt Chan <thewchan@gmail.com>
2020-06-26 10:33:40 -0700

    Render plot on page, interactivity not working yet.

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 """Display a stock ticker visualization based on user input."""
+import json
 from flask import Flask, render_template, request, redirect
+from bokeh.embed import json_item
+from bokeh.resources import CDN
+from ticker import ticker
 
 
 app = Flask(__name__)
+app.vars = {}
+
+
+def create_ticker(symbol):
+    """Render ticker json object from stock symbol."""
+    p = ticker(symbol)
+
+    return p
 
 
 @app.route('/', methods=('GET', 'POST'))
@@ -10,18 +22,37 @@ def index():
     """Render the app's main page."""
     if request.method == 'GET':
         # Default gives IBM's ticker when first loaded.
-        symbol = 'IBM'
-        return render_template('index.html', symbol=symbol)
+        app.vars['symbol'] = 'IBM'
+        p = create_ticker(app.vars['symbol'])
+        p_json = json.dumps(json_item(p, 'myplot'))
+        return render_template('index.html',
+                               symbol=app.vars['symbol'],
+                               p=p,
+                               p_json=p_json,
+                               )
 
     else:
         # User submitted a ticker symbol aka method = 'POST'
-        symbol = request.form['symbol']
-        return render_template('index.html', symbol=symbol)
+        app.vars['symbol'] = request.form['symbol']
+        p = create_ticker(app.vars['symbol'])
+        json.dumps(json_item(p, 'myplot'))
+        return render_template('index.html',
+                               symbol=app.vars['symbol'],
+                               p=p,
+                               p_json=p_json,
+                               )
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/plot')
+def plot():
+    """Render json item from bokeh plot."""
+    p = create_ticker(app.vars['symbol'])
+    return json.dumps(json_item(p, 'myplot'))
 
 
 if __name__ == '__main__':
